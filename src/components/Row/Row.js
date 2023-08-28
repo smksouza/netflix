2a6fc26f2f8609337122729c3828968d6152aedf
Samuel Souza <smksouza.dev@gmail.com>
2023-08-28 13:22:21 -0300

    estilização e criação de componentes

---
+++ src/components/Row/Row.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { getMovies } from "../../api";
+import "./Row.css";
+
+const imageHost = "https://image.tmdb.org/t/p/original/";
+function Row({ title, path, isLarge }) {
+  const [movies, setMovies] = useState([]);
+
+  const fechMovies = async (_path) => {
+    try {
+      const data = await getMovies(_path);
+      setMovies(data?.results);
+    } catch (error) {
+      console.error("fetchMovies error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fechMovies(path);
+  }, [path]);
+
+  return (
+    <div className="row-container">
+      <h2 className="row-header">{title}</h2>
+      <div className="row-cards">
+        {movies?.map((movie) => {
+          return (
+            <img
+              className={`movie-card ${isLarge && "movie-card-large"}`}
+              key={movie.id}
+              src={`${imageHost}${movie.poster_path}`}
+              alt={movie.name}
+            ></img>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
